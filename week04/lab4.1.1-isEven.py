d1cbd1dff8341f0c1f02fcd6a8b46818d7fe988a
ECronin1973 <g00425645@atu.ie>
2023-02-18 20:04:55 +0000

    week4 lab work

---
+++ week04/lab4.1.1-isEven.py
@@ -0,0 +1,10 @@
+#lab4.1.1-isEven.py
+#this program will tell whether an output is odd or even
+#Author: Edward Cronin
+
+number = int(input("enter an integer:"))
+
+if (number % 2) == 0:
+    print (f"{number} is an even number")
+else:
+    print (f"{number} is an odd number")
---
+++ week04/lab4.1.3-gradeRound.py
@@ -0,0 +1,22 @@
+#lab4.1.2-grade.py
+#this program reads in a students percentage
+#and prints out the corresponding grade
+#Author: Edward Cronin
+
+percentage = float(input("Enter the percentage: "))
+roundedNumber = round(percentage)
+print ( '{} rounded is {}'.format(percentage, roundedNumber))
+#print (percentage)
+
+if percentage <0 or percentage > 100:
+    print ("please enter a number between 0 and 100")
+elif percentage <40: #we know it is greater than zero
+    print ("Fail")
+elif percentage <50: #between 40 and 49
+    print ("Pass")
+elif percentage <60: #between 50 and 59
+    print ("Merit2")
+elif percentage <70:#between 60 and 69
+    print ("Merit1")
+else: #the only option left is between 70 and 100
+    print ("Distinction")
