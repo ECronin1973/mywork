677211a17d6e2dc08b4fc387edf013857f82d2a6
ECronin1973 <g00425645@atu.ie>
2023-02-18 16:58:51 +0000

    Week 02 Extra

---
+++ week02/nameAndAge.py
@@ -0,0 +1,13 @@
+#nameAndAge.py
+#This program just prints out a persons name and age - week two extra questions0
+#Author: Edward Cronin
+
+#name = input("Enter your name: ")
+#age =input("Enter you age: ")
+#print(f'Hello {name}, your age is {age}.')
+#commands saved for documenting part 16
+
+name = input("Enter your name: ")
+age =input("Enter you age: ")
+print(f'Hello {name},\t your age is {age}.')
+#commands for step 17
