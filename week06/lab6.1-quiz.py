75ca9ec6a1e8c632dfe634ae4222713fd98e0609
ECronin1973 <g00425645@atu.ie>
2023-03-08 10:05:11 +0000

    adding lab 06 Quiz

---
+++ week06/lab6.1-quiz.py
@@ -0,0 +1,10 @@
+#lab6.1-quiz.py
+# function yo takes one parameter
+# Author: Edward Cronin
+
+def yo(a):
+    return a * 2
+
+# here we are calling the function yo
+# and are passing in an argument, 3
+print(yo(3))
